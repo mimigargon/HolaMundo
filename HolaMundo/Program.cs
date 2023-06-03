892b510f03f4efb42f28eda1f11b637466bb92f5
mimigargon <mireiag77@gmail.com>
2023-06-03 11:32:01 +0200

    Agregar archivos de proyecto.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce una frase (ej: Hola Mundo!): ");
+            string sentence = Console.ReadLine();
+            printSentence(sentence);
+        }
+
+        public static string printSentence (string newSentence)
+        {
+            Console.Write(newSentence);
+            Console.Write("\n");
+            return newSentence;
+        }
+    }
+}
